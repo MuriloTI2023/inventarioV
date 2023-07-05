b20ec6e3e307dab15f01954be9b879e05cc7d69f
MuriloTI2023 <estevammuriloti@gmail.com>
2023-07-05 10:01:35 -0300

    commit inicial

---
+++ src/test/java/com/varejo/inventario/InventarioAlfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varejo.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioAlfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
